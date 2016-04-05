f82676739415529418375dd6785005930ec39304
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-04-05 14:58:09 -0400

    uses gopkg.in for deps

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package gocp
 import (
 	"fmt"
 
-	goperconn "gopkg.in/karrick/goperconn.v1"
+	goperconn "gopkg.in/karrick/goperconn.v1.1.0"
 )
 
 type config struct {
--- pool.go
+++ pool.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"io"
 
-	"github.com/karrick/goperconn"
-	"github.com/karrick/gopool"
+	goperconn "gopkg.in/karrick/goperconn.v1"
+	gopool "gopkg.in/karrick/gopool.v1"
 )
 
 // DefaultSize specifies the number of connections to maintain to a single host for a connection
